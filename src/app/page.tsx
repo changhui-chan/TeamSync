a4732e3118bdbe9bee492cb26b08226124ea4998
changhui-chan <dpfwlfhtep584@gmail.com>
2025-02-04 10:45:48 +0900

    feat: 자동 배포 테스트-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,13 +24,13 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <div>이것도 바뀌냐?</div>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div>이거 바뀌냐?</div>
             <Image
               className="dark:invert"
               src="/vercel.svg"
