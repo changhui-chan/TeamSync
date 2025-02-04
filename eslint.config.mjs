2c56045bd3e84d64b357dea6da541a3cd3b293a4
김지윤 <kimjinewn@gmail.com>
2025-02-05 02:07:41 +0900

    :wrench: chore: ESLint 규칙 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,11 @@
 import { dirname } from 'path';
+
 import { FlatCompat } from '@eslint/eslintrc';
 import pluginQuery from '@tanstack/eslint-plugin-query';
 import configPrettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import { fileURLToPath } from 'url';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,8 +19,46 @@ const eslintConfig = [
   ...pluginQuery.configs['flat/recommended'],
   configPrettier,
   {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-empty-function': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'warn',
+
+      'react/jsx-boolean-value': ['warn', 'never'],
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/self-closing-comp': 'warn',
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+
+      'import/no-unresolved': 'error',
+      'import/newline-after-import': ['warn', { count: 2 }],
+
+      'react-hooks/exhaustive-deps': 'warn',
+      '@next/next/no-img-element': 'warn',
+
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            ['^node:.*', '^(fs|path|crypto)$'],
+
+            ['^react$', '^@?\\w'],
+
+            ['^@/'],
+
+            ['^\\.\\./', '^\\./'],
+
+            ['^.+\\.s?css$', '^.+\\.(png|jpg|svg)$'],
+
+            ['^\\.'],
+          ],
+        },
+      ],
     },
   },
 ];
