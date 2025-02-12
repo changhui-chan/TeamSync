f4746fd3ed3f8dae9fc82d2f6664c0bc9bbb92ea
changhui-chan <dpfwlfhtep584@gmail.com>
2025-02-12 16:52:04 +0900

    ✨feat: useIsDesktop 추가

--- src/hook/useDevice.ts
+++ src/hook/useDevice.ts
@@ -1,5 +1,22 @@
 import { useEffect, useState } from 'react';
 
+export const useIsDesktop = () => {
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 1200);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return isDesktop;
+};
+
 export const useIsMobile = () => {
   const [isMobile, setIsMobile] = useState(false);
 
