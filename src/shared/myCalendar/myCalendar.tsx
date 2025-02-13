becc4f86448a4398a237d9c65b999ecf6569fe90
박지민 <25wlals@gmail.com>
2025-02-13 10:35:15 +0900

    ✨ feat: 캘린더 커스텀 1차

--- src/shared/myCalendar/myCalendar.tsx
+++ src/shared/myCalendar/myCalendar.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import dayjs from 'dayjs';
 import Calendar from 'react-calendar';
 
+// import { BiSolidLeftArrow, BiSolidRightArrow } from 'react-icons/bi';
 import './myCalendar.css';
 
 interface CalendarProps {
@@ -11,12 +13,35 @@ type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
+const PrevIcon = () => {
+  return <div></div>;
+  // return <BiSolidLeftArrow />;
+};
+
+const NextIcon = () => {
+  return <div></div>;
+  // return <BiSolidRightArrow />;
+};
+
 const MyCalendar = ({ className, ...rest }: CalendarProps) => {
   const [value, onChange] = useState<Value>(new Date());
+  console.log(value);
+
+  // onViewChange 에서 next/prev만 남기고 막기
 
   return (
-    <div className={className} {...rest}>
-      <Calendar onChange={onChange} value={value} />
+    <div className={'w-[400px]'} {...rest}>
+      <Calendar
+        onChange={onChange}
+        value={value}
+        locale="en"
+        calendarType="gregory"
+        formatDay={(locale, date) => dayjs(date).format('D')}
+        formatMonthYear={(locale, date) => dayjs(date).format('MMMM YYYY')}
+        formatWeekday={(locale, date) => dayjs(date).format('DDD')}
+        prevLabel={<PrevIcon />}
+        nextLabel={<NextIcon />}
+      />
     </div>
   );
 };
