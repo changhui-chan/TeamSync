eaacd8da20f4bbc4b5731552eb4fb45ef929c740
김지윤 <kimjinewn@gmail.com>
2025-02-19 23:18:47 +0900

    :sparkles: feat: 동적 id 생성 및 렌더링 방식 변경

--- src/shared/modal/Modal.tsx
+++ src/shared/modal/Modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { type ReactNode, useRef } from 'react';
+import { useId } from 'react';
 import { createPortal } from 'react-dom';
 
 import { useEscapeKey } from '@/hook/useEscapeKey';
@@ -11,38 +12,47 @@ import { handleBackdropClick } from '@/util/clickHandlers';
 import { useModal } from './hook/useModal';
 
 export const Modal = ({
+  title,
   children,
   overlay = 'modal-overlay',
   modal = 'modal',
 }: {
+  title: string;
   children: ReactNode;
   overlay?: string;
   modal?: string;
 }) => {
+  const generatedId = useId();
   const modalRef = useRef<HTMLDivElement>(null);
 
-  const { modalState, close } = useModal();
+  const { modalState, close, open } = useModal(generatedId);
 
   useTrapFocus(modalRef);
 
   useEscapeKey(close);
   useReset(close);
 
-  if (!modalState) return null;
-
-  return createPortal(
+  const modalView = modalState && (
     <div
       className={overlay}
       onClick={(e) => handleBackdropClick(e, close)}
       tabIndex={-1}
-      role="dialog"
-      aria-modal="true"
+      role="dialog" //상호작용을 필요로 하는 UI임을 의미
+      aria-modal="true" //모달이 열려 있을 때 다른 콘텐츠와 상호작용을 차단함을 의미
       ref={modalRef}
+      id={generatedId}
     >
       <div className={modal}>
         <div className="modal-content">{children}</div>
       </div>
-    </div>,
-    document.getElementById('modal-root')!
+    </div>
+  );
+
+  return (
+    <>
+      <button onClick={open}>{title}</button>
+      {modalView &&
+        createPortal(modalView, document.getElementById('modal-root')!)}
+    </>
   );
 };
