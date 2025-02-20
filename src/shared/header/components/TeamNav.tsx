07757af49c8146867d8a0703ae2cbbff4c18f310
changhui-chan <dpfwlfhtep584@gmail.com>
2025-02-21 05:47:46 +0900

    ✨feat: TeamNav.tsx 일부 구현

--- src/shared/header/components/TeamNav.tsx
+++ src/shared/header/components/TeamNav.tsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+import { IoIosArrowDown } from 'react-icons/io';
+
+import TeamNavDropdown from './TeamNavDropdown';
+
 const TeamNav = () => {
-  return <div>TeamNav</div>;
+  const [team, setTeam] = useState('경영관리팀');
+  const [teamList, setTeamList] = useState([
+    '경영관리팀',
+    '프로덕트팀',
+    '콘텐츠팀',
+    '마케팅팀',
+  ]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleTeamChangeDropdown = () => {
+    setTeam(teamList[Math.floor(Math.random() * teamList.length)]);
+  };
+
+  return (
+    <div>
+      <div onClick={handleTeamChangeDropdown}>
+        <p>{team}</p>
+        <IoIosArrowDown />
+      </div>
+      {isDropdownOpen && <TeamNavDropdown />}
+    </div>
+  );
 };
 
 export default TeamNav;
