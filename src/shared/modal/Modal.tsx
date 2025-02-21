d4d5a11640d74cde4cce23b4651eed6ce5e88cab
김지윤 <kimjinewn@gmail.com>
2025-02-21 22:20:13 +0900

    :sparkles: feat: 모달 동적 ID 생성 및 메모이제이션 추가

--- src/shared/modal/Modal.tsx
+++ src/shared/modal/Modal.tsx
@@ -2,6 +2,7 @@
 
 import { type ReactNode, useRef } from 'react';
 import { useId } from 'react';
+import React from 'react';
 import { createPortal } from 'react-dom';
 
 import { useEscapeKey } from '@/hook/useEscapeKey';
@@ -11,48 +12,54 @@ import { handleBackdropClick } from '@/util/clickHandlers';
 
 import { useModal } from './hook/useModal';
 
-export const Modal = ({
-  title,
-  children,
-  overlay = 'modal-overlay',
-  modal = 'modal',
-}: {
-  title: string;
-  children: ReactNode;
-  overlay?: string;
-  modal?: string;
-}) => {
-  const generatedId = useId();
-  const modalRef = useRef<HTMLDivElement>(null);
+export const Modal = React.memo(
+  ({
+    title,
+    children,
+    overlay = 'modal-overlay',
+    modal = 'modal',
+  }: {
+    title: string;
+    children: ReactNode;
+    overlay?: string;
+    modal?: string;
+  }) => {
+    const generatedId = useId();
 
-  const { modalState, close, open } = useModal(generatedId);
+    const modalRef = useRef<HTMLDivElement>(null);
 
-  useTrapFocus(modalRef);
+    const { isOpen, close, open } = useModal(generatedId);
 
-  useEscapeKey(close);
-  useReset(close);
+    useTrapFocus(modalRef, isOpen);
+    useEscapeKey(close, isOpen);
+    useReset(close);
 
-  const modalView = modalState && (
-    <div
-      className={overlay}
-      onClick={(e) => handleBackdropClick(e, close)}
-      tabIndex={-1}
-      role="dialog" //상호작용을 필요로 하는 UI임을 의미
-      aria-modal="true" //모달이 열려 있을 때 다른 콘텐츠와 상호작용을 차단함을 의미
-      ref={modalRef}
-      id={generatedId}
-    >
-      <div className={modal}>
-        <div className="modal-content">{children}</div>
+    const modalView = isOpen && (
+      <div
+        className={overlay}
+        onClick={(e) => handleBackdropClick(e, close)}
+        role="dialog" //상호작용을 필요로 하는 UI임을 의미
+        aria-modal="true" //모달이 열려 있을 때 다른 콘텐츠와 상호작용을 차단함을 의미
+        ref={modalRef}
+        id={generatedId}
+        tabIndex={-1}
+      >
+        <div className={modal}>
+          <div className="modal-content">{children}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
 
-  return (
-    <>
-      <button onClick={open}>{title}</button>
-      {modalView &&
-        createPortal(modalView, document.getElementById('modal-root')!)}
-    </>
-  );
-};
+    return (
+      <>
+        <button onClick={open} id={generatedId} tabIndex={-1}>
+          {title}
+        </button>
+        {modalView &&
+          createPortal(modalView, document.getElementById('modal-root')!)}
+      </>
+    );
+  }
+);
+
+Modal.displayName = 'Modal';
