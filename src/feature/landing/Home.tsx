a266ccc97a760e2698cb5873c850a3e9c2ebf29a
changhui-chan <dpfwlfhtep584@gmail.com>
2025-02-25 18:24:20 +0900

    ✨feat: 이미지 반영

--- src/feature/landing/Home.tsx
+++ src/feature/landing/Home.tsx
@@ -35,23 +35,66 @@ const Home = () => {
       <button type="button">지금 시작하기</button>
       <section>
         <div>
-          <div>첫 번째 카드 이미지</div>
+          <Image
+            src="/image/img-home1.svg"
+            alt="홈1 이미지"
+            width={285}
+            height={330}
+          />
           <div>
-            <div>폴더 이미지</div>
-            <p>그룹으로 할 일을 관리해요</p>
+            <div>
+              <Image
+                src="/icon/ic-folderFill.svg"
+                alt="폴더 아이콘"
+                width={24}
+                height={24}
+              />
+            </div>
+            <p>
+              그룹으로
+              <br />할 일을 관리해요
+            </p>
           </div>
         </div>
         <div>
           <div>
-            <div>메일 이미지</div>
-            <p>간단하게 멤버들을 초대해요</p>
+            <div>
+              <Image
+                src="/icon/ic-messageAltFill.svg"
+                alt="메일 아이콘"
+                width={24}
+                height={24}
+              />
+            </div>
+            <p>
+              간단하게 멤버들을
+              <br />
+              초대해요
+            </p>
           </div>
-          <div>두 번째 카드 이미지</div>
+          <Image
+            src="/image/img-home2.svg"
+            alt="홈2 이미지"
+            width={275}
+            height={335}
+          />
         </div>
         <div>
-          <div>세 번째 카드 이미지</div>
+          <Image
+            src="/image/img-home3.svg"
+            alt="홈3 이미지"
+            width={290}
+            height={340}
+          />
           <div>
-            <div>체크 이미지</div>
+            <div>
+              <Image
+                src="/icon/ic-done.svg"
+                alt="체크 아이콘"
+                width={24}
+                height={24}
+              />
+            </div>
             <p>할 일도 간편하게 체크해요</p>
           </div>
         </div>
@@ -61,7 +104,12 @@ const Home = () => {
           <h3>지금 바로 시작해보세요</h3>
           <p>팀원 모두와 같은 방향, 같은 속도로 나아가는 가장 쉬운 방법</p>
         </div>
-        <div>푸터 이미지 영역</div>
+        <Image
+          src="/image/img-footer.svg"
+          alt="푸터 이미지"
+          width={810}
+          height={250}
+        />
       </footer>
     </div>
   );
